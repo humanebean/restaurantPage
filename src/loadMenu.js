2e77d3867d81e7752852af6bec5c4df3611af9c7
Danyal Sheikh <60336684+humanebean@users.noreply.github.com>
2024-02-26 01:56:49 -0600

    changed image paths since the images weren't showing up on github pages

--- src/loadMenu.js
+++ src/loadMenu.js
@@ -30,16 +30,16 @@ function createMenuItem(name,description,imageURL,price){
 
 function createMenuPage(){
     
-    createMenuItem("Aji ifry","Aji Fry is a traditional Japanese home-cooking dish. Butterflied horse mackerel is coated in breadcrumbs and deep-fried to a perfect golden colour. The method of butterflying the fish is unique as it opens the fish from the dorsal side instead of the belly side.","/restaurantPage/images/ajiFry.avif","$12.99");
-    createMenuItem("Bento","This is Bento box that contains sticky rice, sausage, gfried potatoes and grilled leeks.","/restaurantPage/images/bento.webp","$10.99");
-    createMenuItem("Breakfast","Fried eggs and bacon, cooked with Calcifer's special flame.","/restaurantPage/images/breakfast.png","$12.99");
-    createMenuItem("Egg On Toast","A fried egg served on top of a slice of toasted wheat bread.","/restaurantPage/images/eggOnToast.avif","$7.99");
-    createMenuItem("Mackerel","Tender and juicy mackerel served with our special sauce.","/restaurantPage/images/mackerel.avif","$16.99");
-    createMenuItem("Onigiri","This is onigiri, a rice ball made from white rice formed into circular shape and wrapped in nori.","/restaurantPage/images/onigiri.webp","$5.99");
-    createMenuItem("Pancakes","Delicious made to order pancakes served with sausage and strawberries.","/restaurantPage/images/pancakes.avif","$8.99");
-    createMenuItem("Siberia","Castella Siberia, a cake extremely popular in Japan in the 1920s and 1930s, made from a sort of sponge named kasutera or castella and filled with a layer of red bean paste made from the famous azuki.","/restaurantPage/images/siberia.png","$4.99");
-    createMenuItem("Spaghetti","Buttery spaghetti lovingly slathered in our special sauce and served with meatballs.","/restaurantPage/images/spaghetti.jpg","$11.99");
-    createMenuItem("All you can eat special","All you can eat. No one will stop you.","/restaurantPage/images/image01.jpg","$100");
+    createMenuItem("Aji ifry","Aji Fry is a traditional Japanese home-cooking dish. Butterflied horse mackerel is coated in breadcrumbs and deep-fried to a perfect golden colour. The method of butterflying the fish is unique as it opens the fish from the dorsal side instead of the belly side.","../images/ajiFry.avif","$12.99");
+    createMenuItem("Bento","This is Bento box that contains sticky rice, sausage, gfried potatoes and grilled leeks.","../images/bento.webp","$10.99");
+    createMenuItem("Breakfast","Fried eggs and bacon, cooked with Calcifer's special flame.","../images/breakfast.png","$12.99");
+    createMenuItem("Egg On Toast","A fried egg served on top of a slice of toasted wheat bread.","../images/eggOnToast.avif","$7.99");
+    createMenuItem("Mackerel","Tender and juicy mackerel served with our special sauce.","../images/mackerel.avif","$16.99");
+    createMenuItem("Onigiri","This is onigiri, a rice ball made from white rice formed into circular shape and wrapped in nori.","../images/onigiri.webp","$5.99");
+    createMenuItem("Pancakes","Delicious made to order pancakes served with sausage and strawberries.","../images/pancakes.avif","$8.99");
+    createMenuItem("Siberia","Castella Siberia, a cake extremely popular in Japan in the 1920s and 1930s, made from a sort of sponge named kasutera or castella and filled with a layer of red bean paste made from the famous azuki.","../images/siberia.png","$4.99");
+    createMenuItem("Spaghetti","Buttery spaghetti lovingly slathered in our special sauce and served with meatballs.","../images/spaghetti.jpg","$11.99");
+    createMenuItem("All you can eat special","All you can eat. No one will stop you.","../images/image01.jpg","$100");
 }
 
 export default createMenuPage;
