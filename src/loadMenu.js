c131c820942f00eb5673e13bdf78df68a97324f7
Danyal Sheikh <60336684+humanebean@users.noreply.github.com>
2024-02-25 23:20:55 -0600

    finished up the menu page. Added custom images and descriptions for each menu item and styled them with css. Added a transparent background, a border, adjusted the margins, and used flexbox to center the items

--- src/loadMenu.js
+++ src/loadMenu.js
@@ -1,4 +1,4 @@
-function createMenuItem(name,description,imageURL){
+function createMenuItem(name,description,imageURL,price){
     
     let container = document.createElement('div');
     let content = document.getElementById("content");
@@ -11,6 +11,10 @@ function createMenuItem(name,description,imageURL){
     itemName.innerHTML=name;
     itemName.classList.add("menuItemName");
 
+    let itemPrice = document.createElement('p');
+    itemPrice.innerHTML = price;
+    itemPrice.classList.add("itemPrice");
+
     let itemDesc = document.createElement('p');
     itemDesc.innerHTML = description;
 
@@ -18,7 +22,7 @@ function createMenuItem(name,description,imageURL){
     image.src=imageURL;
     image.classList.add("menuImage");
     
-    nameAndImage.append(itemName,image);
+    nameAndImage.append(itemName,image,itemPrice);
     container.append(nameAndImage,itemDesc);
     content.append(container);
     console.log("loadMenu");
@@ -26,16 +30,16 @@ function createMenuItem(name,description,imageURL){
 
 function createMenuPage(){
     
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
-    createMenuItem("noface","This is no face","/restaurantPage/images/image01.jpg");
+    createMenuItem("Aj ifry","Aji Fry) is a traditional Japanese home-cooking dish. Butterflied horse mackerel is coated in breadcrumbs and deep-fried to a perfect golden colour. The method of butterflying the fish is unique as it opens the fish from the dorsal side instead of the belly side.","/restaurantPage/images/ajiFry.avif","$12.99");
+    createMenuItem("Bento","This is Bento box that contains sticky rice, sausage, gfried potatoes and grilled leeks.","/restaurantPage/images/bento.webp","$10.99");
+    createMenuItem("Breakfast","Fried eggs and bacon, cooked with Calcifer's special flame.","/restaurantPage/images/breakfast.png","$12.99");
+    createMenuItem("Egg On Toast","A fried egg served on top of a slice of toasted wheat bread.","/restaurantPage/images/eggOnToast.avif","$7.99");
+    createMenuItem("Mackerel","Tender and juicy mackerel served with our special sauce.","/restaurantPage/images/mackerel.avif","$16.99");
+    createMenuItem("Onigiri","This is onigiri, a rice ball made from white rice formed into circular shape and wrapped in nori.","/restaurantPage/images/onigiri.webp","$5.99");
+    createMenuItem("Pancakes","Delicious made to order pancakes served with sausage and strawberries.","/restaurantPage/images/pancakes.avif","$8.99");
+    createMenuItem("Siberia","Castella Siberia, a cake extremely popular in Japan in the 1920s and 1930s, made from a sort of sponge named kasutera or castella and filled with a layer of red bean paste made from the famous azuki.","/restaurantPage/images/siberia.png","$4.99");
+    createMenuItem("Spaghetti","Buttery spaghetti lovingly slathered in our special sauce and served with meatballs.","/restaurantPage/images/spaghetti.jpg","$11.99");
+    createMenuItem("All you can eat special","All you can eat. No one will stop you.","/restaurantPage/images/image01.jpg","$100");
 }
 
 export default createMenuPage;
